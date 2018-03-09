57d70cedc3292251efcc6d0144c32b6799a9647f
Erykyu <33733919+Erykyu@users.noreply.github.com>
2018-03-09 16:57:04 +0100

    Ajout de modules + pages Home et 404
    Importation de Angular Material, routage, et pages Home + 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+/**
+ * MODULES
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// MATERIAL
 
 
+
+/**
+ * COMPONENTS
+ */
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+/**
+ * ROUTING
+ */
+const routes = [
+    { path: '/', component: HomeComponent },
+    { path: '**', component: NotFoundComponent }
+];
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        NotFoundComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot(routes),
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
